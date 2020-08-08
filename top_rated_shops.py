38185ef65bdbdadb2176f85eeca805e571f21429
smatthew <shanto12@gmail.com>
2020-08-09 08:47:09 +1000

    d

--- top_rated_shops.py
+++ top_rated_shops.py
@@ -15,11 +15,25 @@ gmaps = googlemaps.Client(key=API_KEY)
 
 # Look up an address with reverse geocoding
 # reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+params = dict()
+params["query"] = "coffee"
+params["location"] = "Dee Why"
+params["radius"] = 1*1000
+params["open_now"] = True
+
+results_list = []
 
 
-coffee_places = gmaps.places(query="coffee", location="Dee Why", radius=1000)
+while True:
+    coffee_places = gmaps.places(**params)
 
-print(coffee_places)
+    results_list.extend(coffee_places["results"])
+    if coffee_places["next_page_token"]:
+        params["next_page_token"] = coffee_places["next_page_token"]
+    else:
+        del params["next_page_token"]
+
+print(results_set)
 
 # Request directions via public transit
 # now = datetime.now()
